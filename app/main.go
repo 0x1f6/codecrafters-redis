e84028d84dccfc6dc56cf97a498d4fc4e59e8a82
0x1f6 <178943044+0x1f6@users.noreply.github.com>
2026-04-05 17:18:31 +0000

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
@@ -10,22 +11,57 @@ import (
 var _ = net.Listen
 var _ = os.Exit
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment the code below to pass the first stage
-	//
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+func main() {
+	fmt.Println("=== Program starting... ===")
+
+	// Setup listener
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer listener.Close()
+
+	for {
+        // Block until we receive an incoming connection
+        conn, err := listener.Accept()
+        if err != nil {
+            fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+            //continue
+        }
+
+        // Handle client connection
+        handleClient(conn)
+		break
+    }
+}
+
+
+func handleClient(conn net.Conn) {
+	// Ensure we close the connection after we're done
+    defer conn.Close()
+
+	pongStr := []byte("+PONG\r\n")
+	buf := make([]byte, 1024)
+
+	for {
+		bytesReceived, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading from connection: ", err.Error())
+			break
+		}
+
+		// Ignore empty commands
+		if bytesReceived == 0 {
+			continue
+		}
+
+		_, err = conn.Write(pongStr)
+		if err != nil {
+			fmt.Println("Error writing to connection: ", err.Error())
+			break
+		}
 	}
-	conn.Write([]byte("+PONG\r\n"))
-	conn.Close()
 }
